3f4a8985e42c9e80ff6c2f7371a30d3927df26af
Thao Nguyen <thao031103@gmail.com>
2025-09-29 08:26:37 -0400

    Implement print fwd table mode

--- proj2.cc
+++ proj2.cc
@@ -1,16 +1,26 @@
 #include <iostream>
 #include <cstdlib>
 #include <unistd.h>
+#include <cstdint>
+#include <cstdio>
+#include <arpa/inet.h>
 
 #define ARG_PACKET_PRINT  0x1
 #define ARG_FWD_TABLE_PRINT   0x2
 #define ARG_SIMULATION 0x4
 #define ARG_FWD_FILE 0x8
 #define ARG_TRACE_FILE  0x10
+#define FWD_TABLE_ENTRY_SZ 8
 
 unsigned short cmd_line_flags = 0;
 char *fwd_file = NULL;
 char *trace_file = NULL;
+
+struct ForwardingTableEntry {
+    uint32_t ip;
+    uint16_t prefix_len;
+    uint16_t interface;
+};
 
 void usage (char *progname)
 {
@@ -67,8 +77,36 @@ void printPacket(char *trace_file) {
 }
 
 void printForwardingTable(char *fwd_file) {
-    std::cout << "Printing forwarding table from file: " << fwd_file << std::endl;
-    // Implementation of forwarding table printing goes here
+    // Print rules in forwarding table file in the following format:
+    //          ip prefix_len interface
+    // • ip: This is the IPv4 address from the forwarding table printed in dotted-quad notation. That is, four
+    // unpadded decimal integers separated by periods (“.”). E.g., “192.168.10.54”.
+    // • prefix len: The number of bits the router will use when comparing the IP address prefix in the rule
+    // with the destination IP address in incoming packets.
+    // • interface: The interface number on which to forward traffic matching the rule’s IP address and prefix.
+    // Examples:
+    // 10.0.0.0 8 1
+    ForwardingTableEntry entry;
+    FILE * file = fopen(fwd_file, "rb");
+
+    if (file == nullptr) {
+        fprintf(stderr, "Error: could not open file %s \n", fwd_file);
+        exit(1);
+    }
+
+    // Read each entry in the forwarding table file
+    while (fread(&entry, FWD_TABLE_ENTRY_SZ, 1, file) == 1) {
+        // Turn fields into host byte order first
+        uint32_t ip = ntohl(entry.ip);
+        uint8_t quad1 = (ip >> 24) & 0xFF;
+        uint8_t quad2 = (ip >> 16) & 0xFF;
+        uint8_t quad3 = (ip >> 8) & 0xFF;
+        uint8_t quad4 = ip & 0xFF;
+        uint16_t prefix_len = ntohs(entry.prefix_len);
+        uint16_t interface = ntohs(entry.interface);
+        printf("%u.%u.%u.%u %u %u\n", quad1, quad2, quad3, quad4, prefix_len, interface);
+    }
+    fclose(file);
 }
 
 void simulation(char *fwd_file, char *trace_file) {
@@ -81,21 +119,20 @@ int main (int argc, char *argv [])
 {
     parseargs (argc,argv);
 
-    if (cmd_line_flags == ARG_PACKET_PRINT) {
+    if (cmd_line_flags & ARG_PACKET_PRINT) {
         if (!(cmd_line_flags & ARG_TRACE_FILE)) {
             fprintf (stderr,"error: -p requires -t trace_file\n");
             usage (argv [0]);
         }
         printPacket(trace_file);
     }
-    else if (cmd_line_flags == ARG_FWD_TABLE_PRINT) {
+    else if (cmd_line_flags & ARG_FWD_TABLE_PRINT) {
         if (!(cmd_line_flags & ARG_FWD_FILE)) {
             fprintf (stderr,"error: -r requires -f forward_file\n");
             usage (argv [0]);
         }
         printForwardingTable(fwd_file);
     }
-    
     else if (cmd_line_flags & ARG_SIMULATION) {
         if (!(cmd_line_flags & ARG_FWD_FILE) && !(cmd_line_flags & ARG_TRACE_FILE)) {
             fprintf (stderr,"error: -s requires -f forward_file and -t trace_file\n");
